64f8c9ea358d0d385ec8ef00f64a63c05e740362
itcampusdigital <it.campus.back@gmail.com>
2023-07-29 09:00:03 +0700

    update config

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://psikologanda.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -69,7 +69,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Jakarta',
 
     /*
     |--------------------------------------------------------------------------
